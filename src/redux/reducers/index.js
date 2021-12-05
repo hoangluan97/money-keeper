19ff611ce4b761261ef1338c68cea7466dc67047
Joejoeflyn <thaitainguyen336@gmail.com>
2021-12-05 19:43:10 +0700

    problem with scss

--- src/redux/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import postReducer from "./postReducer";
-const reducers = combineReducers({
-    posts: postReducer
-});
-export default {state, action} => reducers(state, action);
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducers';
-
-const middleware = [thunk];
-
-if(process.envn.NODE_ENV !== "production"){
-    middleware.push()
-}
